c98ce6a5c46ddc89d64916632ba422d1b17178d8
Jobin T V <jobinvarghese18@gmail.com>
2020-04-07 13:51:03 -0500

    fourt commit

--- src/Application.js
+++ src/Application.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Axios from 'axios'
+import '../src/app.css'
 
 class Application extends React.Component{
     constructor(){
@@ -51,7 +52,7 @@ class Application extends React.Component{
                    <input type='text' name='contact' id='contact' value = {this.state.contact} onChange={this.handleInput}/>
                    <br/>
                    <label htmlFor='job'>Applying for Job</label>
-                   <select id='job' name='job' onChange={this.handleInput} defaultValue={this.state.job} >
+                   <select className="styled-select" id='job' name='job' onChange={this.handleInput} defaultValue={this.state.job} >
                        <option value='Front-End Developer'>Front.End Developer</option>
                        <option value='Node.js Developer'>Node.js Developer</option>
                        <option value='MEAN Stack Developer'>MEAN Stack Developer</option>
@@ -64,7 +65,7 @@ class Application extends React.Component{
                    <label htmlFor='skills'>Technical Skills</label>
                    <input type='textarea' name='skills' id='skills' value ={this.state.skills} onChange={this.handleInput}/>
                    <br/>
-                   <input type='submit'  value='Send Application'/>
+                   <input type='submit' className="myButton" value='Send Application'/>
                 </form>
 
             </div>
